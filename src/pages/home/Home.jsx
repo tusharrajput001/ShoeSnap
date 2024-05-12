d419891e1d59fba3249963271c156aa625fdef23
tusharrajput001 <devoptushar@gmail.com>
2024-05-12 23:40:51 +0530

    login page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,33 @@
-import React, { useContext } from 'react'
-import Layout from '../../components/layout/Layout'
-import myContext from '../../context/data/myContext'
-import HeroSection from '../../components/heroSection/HeroSection'
-import ProductCard from '../../components/productCard/ProductCard'
-import Track from '../../components/track/Track'
-import Testimonial from '../../components/testimonial/Testimonial'
-import { Link } from 'react-router-dom'
-import HomeSearch from '../../components/filter/HomeSearch'
-import Partners from '../../components/Partners/partners'
-import ClientReviews from '../../components/ClientReviews/ClientReviews'
+import React, { useContext } from "react";
+import Layout from "../../components/layout/Layout";
 
+import HeroSection from "../../components/heroSection/HeroSection";
+import ProductCard from "../../components/productCard/ProductCard";
+
+import { Link } from "react-router-dom";
+import HomeSearch from "../../components/filter/HomeSearch";
+import Partners from "../../components/Partners/partners";
+import ClientReviews from "../../components/ClientReviews/ClientReviews";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
-      <HomeSearch/> 
+      <HomeSearch />
       <ProductCard />
       <div className="flex justify-center -mt-10 mb-4">
-        <Link to={'/allproducts'}>
-          <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
+        <Link to={"/allproducts"}>
+          <button className=" bg-gray-300 px-5 py-2 rounded-xl">
+            See more
+          </button>
         </Link>
       </div>
       {/* <Track /> */}
-      <Partners/>
-      <ClientReviews/>
+      <Partners />
+      <ClientReviews />
       {/* <Testimonial /> */}
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,14 +1,14 @@
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import myContext from '../../context/data/myContext'
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import myContext from '../../context/data/myContext';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../fireabase/FirebaseConfig';
 import { toast } from 'react-toastify';
 import Loader from '../../components/loader/Loader';
 
 function Login() {
-    const context = useContext(myContext)
-    const {loading, setLoading} = context;
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,9 +16,9 @@ function Login() {
     const navigate = useNavigate();
 
     const login = async () => {
-        setLoading(true)
+        setLoading(true);
         try {
-            const result = await signInWithEmailAndPassword(auth,email,password);
+            const result = await signInWithEmailAndPassword(auth, email, password);
             toast.success("Login successful", {
                 position: "top-right",
                 autoClose: 2000,
@@ -28,57 +28,58 @@ function Login() {
                 draggable: true,
                 progress: undefined,
                 theme: "colored",
-              })
-            localStorage.setItem('user', JSON.stringify(result))
-            navigate('/')
-            setLoading(false)
-            
+            });
+            localStorage.setItem('user', JSON.stringify(result));
+            navigate('/');
+            setLoading(false);
+
         } catch (error) {
-            console.log(error)
-            setLoading(loading)
+            console.log(error);
+            setLoading(false);
         }
 
     }
-   
+
     return (
-        <div className='flex justify-center items-center h-screen'>
-            {loading && <Loader />}
-            <div className='bg-gray-800 px-10 py-10 rounded-xl w-full max-w-md'>
-                <div className="mb-8">
-                    <h1 className='text-center text-white text-3xl font-bold mb-4'>Login</h1>
-                </div>
-                <div className="mb-4">
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        name='email'
-                        className='bg-gray-600 mb-4 px-3 py-2 w-full rounded-lg text-white placeholder-text-gray-200 outline-none'
-                        placeholder='Email'
-                    />
-                </div>
-                <div className="mb-4">
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='bg-gray-600 mb-4 px-3 py-2 w-full rounded-lg text-white placeholder-text-gray-200 outline-none'
-                        placeholder='Password'
-                    />
-                </div>
-                <div className='mb-4'>
-                    <button
-                        onClick={login}
-                        className='bg-customOrange w-full text-black font-bold px-3 py-2 rounded-lg'>
-                        Login
-                    </button>
-                </div>
-                <div>
-                    <h2 className='text-white text-center'>Don't have an account <Link className='text-orange-500 font-bold' to={'/signup'}>Signup</Link></h2>
+        <section className="vh-100">
+            <div className="container-fluid h-custom">
+                <div className="row d-flex mt-40 mb-60 justify-content-center align-items-center h-100">
+                    <div className="col-md-9 col-lg-6 col-xl-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                            className="img-fluid" alt="Sample image" />
+                    </div>
+                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                        <form>
+
+                            <h1 className='text-5xl font-bold text-center m-16'>Sign in</h1>
+
+                            <div className="form-outline mb-4">
+                                <input type="email" id="form3Example3" className="form-control form-control-lg"
+                                    placeholder="Enter a valid email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                               
+                            </div>
+
+                            <div className="form-outline mb-3">
+                                <input type="password" id="form3Example4" className="form-control form-control-lg"
+                                    placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                
+                            </div>
+
+                            
+
+                            <div className="text-center text-lg-start mt-4 pt-2">
+                                <button type="button" data-mdb-button-init data-mdb-ripple-init className="btn bg-customOrange btn-primary btn-lg"
+                                    style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }} onClick={login}>Login</button>
+                                <p className="large fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link to="/signup"
+                                    className="link-danger">Register</Link></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </section>
     )
 }
 
-export default Login
+export default Login;
