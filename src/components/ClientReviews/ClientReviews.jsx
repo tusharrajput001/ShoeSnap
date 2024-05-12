efaea467c378d12ae250207d199f99ed38c9eca5
tusharrajput001 <devoptushar@gmail.com>
2024-05-12 20:25:46 +0530

    changes on ReviewCard

--- src/components/ClientReviews/ClientReviews.jsx
+++ src/components/ClientReviews/ClientReviews.jsx
@@ -1,19 +1,65 @@
-import React from 'react'
-import "./ClientReviews.css"
-import Review_Card from './Review_Card'
-
+import React from "react";
+import "./ClientReviews.css";
+import Review_Card from "./Review_Card";
 
 function ClientReviews() {
   return (
-    <div className='bg-rev'>
-    <div className='reviews-container'>
-        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"6 months after joining I lost 7kg and I'm getting stronger and getting more Positivity in Lifestyle"} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=male"}/>
-        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"I've gotten so much stronger after joining It's insane. My Deadlift increased to 50kgs."} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=female"}/>
-        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"While I am passionate about health and Fitness this Gym is more like a community to me than anything!"} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=pixel"}/>
+    <div className="bg-rev">
+      <div className="reviews-container">
+        <Review_Card
+          stars_icon={
+            <i class="fa-solid fa-star">
+              <i class="fa-solid fa-star">
+                <i class="fa-solid fa-star">
+                  <i class="fa-solid fa-star">
+                    <i class="fa-solid fa-star"></i>
+                  </i>
+                </i>
+              </i>
+            </i>
+          }
+          review_desc={
+            "Shoesnap has the best selection of stylish and durable shoes for every occasion!"
+          }
+          avatar_link={"https://xsgames.co/randomusers/avatar.php?g=male"}
+        />
+        <Review_Card
+          stars_icon={
+            <i class="fa-solid fa-star">
+              <i class="fa-solid fa-star">
+                <i class="fa-solid fa-star">
+                  <i class="fa-solid fa-star">
+                    <i class="fa-solid fa-star"></i>
+                  </i>
+                </i>
+              </i>
+            </i>
+          }
+          review_desc={
+            "ShoeSnap exceeded my expectations with their wide range of footwear options!"
+          }
+          avatar_link={"https://xsgames.co/randomusers/avatar.php?g=female"}
+        />
+        <Review_Card
+          stars_icon={
+            <i class="fa-solid fa-star">
+              <i class="fa-solid fa-star">
+                <i class="fa-solid fa-star">
+                  <i class="fa-solid fa-star">
+                    <i class="fa-solid fa-star"></i>
+                  </i>
+                </i>
+              </i>
+            </i>
+          }
+          review_desc={
+            "Best Sneaker collection and quality is also top-notch!"
+          }
+          avatar_link={"https://xsgames.co/randomusers/avatar.php?g=pixel"}
+        />
+      </div>
     </div>
-    </div>
-  )
-
+  );
 }
 
-export default ClientReviews
+export default ClientReviews;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import './footer.css'
-import { Link as ScrollLink } from 'react-scroll';
+  import React from 'react'
+  import './footer.css'
+  import {Link } from 'react-router-dom'
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faInstagram, faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+  function Footer() {
+    return (
+      <>
+      <footer className='footer-sec'>
+          <h1 className='footer-head'>ShoeSnap ©️</h1>
+          <nav className='footer-nav'>
+              <a><Link to='/'>Home</Link></a> 
+              <a><Link to='/allproducts'>Explore</Link></a>
+              <a><Link to='/order'>MyOrders</Link></a>
+              <a><Link to='/Contact'>Contact Us</Link></a>
+          </nav>
+          <div className='foot-list'>
+            <FontAwesomeIcon icon={faInstagram} className="fa-2x icons-cols" />
+            <FontAwesomeIcon icon={faFacebook} className="fa-2x icons-cols" />
+            <FontAwesomeIcon icon={faTwitter} className="fa-2x icons-cols" />
+            <FontAwesomeIcon icon={faYoutube} className="fa-2x icons-cols" />
+            
+        </div>
+
+
+          <p className='copyright-sec'>© 2024 ShoeSnap. All rights reserved.</p>
+      </footer>
 
 
 
-function Footer() {
-  return (
-    <>
-     <footer className='footer-sec'>
-        <h1 className='footer-head'>ShoeSnap ©️</h1>
-        <nav className='footer-nav'>
-            <a><ScrollLink to='Home' smooth={true} duration={500}>Home</ScrollLink></a>
-            <a><ScrollLink to='All Products' smooth={true} duration={500}>All Products</ScrollLink></a>
-            <a><ScrollLink to='Order' smooth={true} duration={500}>Order</ScrollLink></a>
-            <a><ScrollLink to='Contact Us' smooth={true} duration={500}>Contact Us</ScrollLink></a>
-        </nav>
-            <div className='foot-list'>
-                        <a><i class="fa-brands fa-instagram fa-2xl icons-cols"></i></a>
-                        <a><i class="fa-brands fa-facebook fa-2xl icons-cols"></i></a>
-                        <a><i class="fa-brands fa-twitter fa-2xl icons-cols"></i></a>
-                        <a><i class="fa-brands fa-youtube fa-2xl icons-cols"></i></a>
-            </div> 
-        
 
+      </>
+    )
+  }
 
-        <p className='copyright-sec'>© 2024 ShoeSnap. All rights reserved.</p>
-    </footer>
-
-
-
-
-    </>
-  )
-}
-
-export default Footer
+  export default Footer
