feec1a650487fc997f549a97ee18057a37fa21e8
tusharrajput001 <devoptushar@gmail.com>
2024-05-20 17:03:48 +0530

    Account page

--- src/pages/Account/account.jsx
+++ src/pages/Account/account.jsx
@@ -16,35 +16,43 @@ function Account() {
   const { currentUser } = useContext(MyContext);
   const userName = currentUser ? currentUser.name : "Name"; 
   return (
-    <>
-    <Navbar/>
-    <div className="fullScreen">
-      <div className="main-container">
-        <div className="profileContainer">
-          <div className="profileImg">
-            <img
-              src="https://static.vecteezy.com/system/resources/thumbnails/005/544/718/small_2x/profile-icon-design-free-vector.jpg"
-              alt="Profile"
-            />
-          </div>
-          <div className="profileName">
-            <h1>{userName}</h1>
-          </div>
-          <div>
-            <button className="btnLogout" onClick={logout}>Logout</button>
-          </div>
+<>
+  <Navbar/>
+  <div className="fullScreen">
+    <div className="main-container">
+      <div className="profileContainer">
+        <div className="profileImg">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/005/544/718/small_2x/profile-icon-design-free-vector.jpg"
+            alt="Profile"
+          />
         </div>
-      </div>
-      <div className="options-container">
+        <div className="profileName">
+          <h1>{userName}</h1>
+        </div>
         <div>
-          <Link to="/cart">Bag</Link>
+          <button className="btnLogout" onClick={logout}>Logout</button>
         </div>
-        <div><Link to="/order">My Orders</Link></div>
-        <div><Link to="/allproducts">Explore</Link></div>
       </div>
     </div>
-    <Footer/>
-    </>
+    <div className="options-container">
+      <div>
+        <Link to="/cart"></Link>
+        <h1>Bag</h1>
+      </div>
+      <div>
+        <Link to="/order"></Link>
+        <h1>My Orders</h1>
+      </div>
+      <div>
+        <Link to="/allproducts"></Link>
+        <h1>Explore</h1>
+      </div>
+    </div>
+  </div>
+  <Footer/>
+</>
+
   );
 }
 
